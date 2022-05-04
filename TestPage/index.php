90b018a05959cb854da7cfb7e6d0351e37ab3703
Leo Yu Lee <leeleoy@dunwoody.edu>
2022-05-04 12:07:56 +0000

    created test page to test php

---
+++ TestPage/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Add Course</title>
+<link rel="stylesheet" href="../stylesheet.css">
+</head>
+
+<body>
+	<h2>Header</h2>
+	<br><br>
+	<?php
+        echo "It works"
+    ?>
+<br><br><a href="../index.html" title="Home Page" target="_parent">Return to Home</a>
+</body>
+</html>

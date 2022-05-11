9cd8e4ffa30183c475c08a8abed0a17ce261bb57
Leo <56608247+leoyulee@users.noreply.github.com>
2022-05-11 10:34:57 -0500

    Fix incorrect website being updated

--- managecourses.php
+++ managecourses.php
@@ -16,7 +16,7 @@
 
 <body>
 	<h2>Course Management</h2>
-	<form action="managecourses.php">
+	<form action="deletecourse.php">
         <?php
             $servername = "localhost";  //mysql is on the same host as apache (not realistic but whatevs)
             $username = "admin1234";    //username for database
